870df78394d3700e00a1c76d9e6cc8a3ea2fdd93
Emilio Zanetti <zanettiemilio@hotmail.com>
2017-11-20 12:22:22 +0800

    seconda prova gitHUb

--- beataTe/js/controllers.js
+++ beataTe/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('app.controllers', [])
-  
+  //this is my second attempt
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -7,13 +7,13 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('beataTeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-   
-   
+
+
 
 
 }
@@ -22,7 +22,7 @@ function ($scope, $stateParams) {
 
 
 ])
-   
+
 .controller('yourMenuOrEventCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -35,7 +35,7 @@ function ($scope, $stateParams) {
 
 
  ])
-   
+
 .controller('page29Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -43,7 +43,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('aboutBeataTeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -51,7 +51,7 @@ function ($scope, $stateParams) {
 
 
 }])
-      
+
 .controller('ourMenuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -59,12 +59,12 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('eventsCtrl', ['$scope', '$stateParams', '$firebaseArray', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $firebaseArray) {
-    
+
     $scope.data = {
         'id': '0',
         'event0': '',
@@ -80,24 +80,24 @@ function ($scope, $stateParams, $firebaseArray) {
         'phone':''
 
     }
-    
+
       var ref = firebase.database().ref().child("Martedi");
       // create a synchronized array
       $scope.days = $firebaseArray(ref);
- 
-  
+
+
 
       }
 
 
 
 ])
-   
+
 .controller('page10Ctrl', ['$scope', '$stateParams', '$firebaseArray', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $firebaseArray) {
-    
+
     $scope.data = {
         'id': '0',
         'event0': '',
@@ -113,37 +113,37 @@ function ($scope, $stateParams, $firebaseArray) {
         'phone':''
 
     }
-    
+
       var ref = firebase.database().ref().child("Martedi");
       // create a synchronized array
       $scope.days = $firebaseArray(ref);
- 
-  
+
+
 
       }
 
 
 
 ])
-   
+
 .controller('myEventCtrl', ['$scope', '$stateParams', 'Survey', '$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, Survey, $ionicPopup) {
-    
-    
+
+
     $scope.data = {
-    
+
     'name' : $stateParams.name,
     'event': $stateParams.event,
     'food': $stateParams.food,
     'phone':  $stateParams.phone
 
-   
+
     };
 
     $scope.submitting = false;
-    
+
     $scope.submit = function(){
         $scope.submitting = true;
         Survey.add($scope.data).then(function(){
@@ -153,42 +153,42 @@ function ($scope, $stateParams, Survey, $ionicPopup) {
         event: '',
         food:''
 
-    
+
             }
-            
+
             $scope.submitting = false;
-            
+
             $ionicPopup.alert({
                 title: '謝謝您!',
                 template: '我們收到了您的資料.'
             });
-            
-            
-        
+
+
+
             //$state.go('yourOrder', $scope.fields);
         })
-    
+
 }
 }])
-   
+
 .controller('page11Ctrl', ['$scope', '$stateParams', 'Survey', '$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, Survey, $ionicPopup) {
-    
-    
+
+
     $scope.data = {
-    
+
     'name' : $stateParams.name,
     'event': $stateParams.event,
     'food': $stateParams.food,
     'phone':  $stateParams.phone
 
-   
+
     };
 
     $scope.submitting = false;
-    
+
     $scope.submit = function(){
         $scope.submitting = true;
         Survey.add($scope.data).then(function(){
@@ -198,21 +198,20 @@ function ($scope, $stateParams, Survey, $ionicPopup) {
         event: '',
         food:''
 
-    
+
             }
-            
+
             $scope.submitting = false;
-            
+
             $ionicPopup.alert({
                 title: '謝謝您!',
                 template: '我們收到了您的資料.'
             });
-            
-            
-        
+
+
+
             //$state.go('yourOrder', $scope.fields);
         })
-    
+
 }
 }])
- 
