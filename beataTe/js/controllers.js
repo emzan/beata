d6f8fb2cb9ae381d6e516d7aa2674f2d308fd216
Emilio Zanetti <zanettiemilio@hotmail.com>
2017-11-20 12:23:36 +0800

    thrd attempt

--- beataTe/js/controllers.js
+++ beataTe/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('app.controllers', [])
-  //this is my second attempt
+  //this is my third attempt
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
